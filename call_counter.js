6860c6c86ee0438c73f3b246edac27d311c97945
Javier Quintana Q <xaviqq7@gmail.com>
2018-09-17 15:47:51 -0400

    version 0.1.16

--- call_counter.js
+++ call_counter.js
@@ -1,6 +1,6 @@
 var internal_call_counter = 0;
 function count_call() {
     ++internal_call_counter;
-    console.log('You have made ' + internal_call_counter + ' calls!');
+    console.log('Has hecho ' + internal_call_counter + ' llamadas!');
 }
 module.exports = count_call;
