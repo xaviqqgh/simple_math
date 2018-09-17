06c1f45dd265eefc46ae3178c30e05ac497c6ba3
Javier Quintana Q <xaviqq7@gmail.com>
2018-09-17 18:17:13 -0400

    cambio de main

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var fs = require('fs');
-var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
+var lib = path.join(path.dirname(fs.realpathSync(__filename)), '');
 var simple = require(lib + '/simple_math.js');
 var advanced = require(lib + '/advanced_math.js');
 module.exports = {
